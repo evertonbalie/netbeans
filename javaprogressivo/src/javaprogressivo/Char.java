44baf43687014c91a2c6f09ee30480cd3d5e938e
teste <teste@PCTON>
2023-04-25 23:50:50 -0300

    alteração da classe java

---
+++ javaprogressivo/src/javaprogressivo/Char.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaprogressivo;
+
+/**
+ *
+ * @author teste
+ */
+public class Char {
+    
+    
+  
+    public static void main(String[] args){
+        /*char letra_J = 74;
+        char letra_P = 6;
+        char letra_Q = 81;
+        char letra_R = 82;
+        char letra_S = 83;
+        System.out.printf("%c %c %c %c %c\n",letra_J, letra_P,letra_Q,letra_R,letra_S);
+   */
+        int numero_J = 74;
+        char letra_J = (char) numero_J;
+        char letra_P = 80;
+        char letra_i = 'i';
+        System.out.printf("%c%c%c%c %c%c%c%c%c%c%c%c%c%c%c\n",
+                                    letra_J,97,118,97, letra_P, 'r',
+                                    111,103,114,101,'s','s',letra_i,
+                                    118, 111);
+    }
+}
+    
+    
+    
+
+
